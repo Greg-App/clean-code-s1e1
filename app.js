d95cd1be12a163c963f997ee85ffc18e0ec6d08e
Greg App <greg358@mail.ru>
2022-10-30 01:03:04 +0300

    refactor: rename elements and modificator  class according to rule advanced 2.1-BEM

--- app.js
+++ app.js
@@ -90,11 +90,11 @@ var editTask=function(){
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".todo-list-section__list-item-edit-btn");
-    var containsClass=listItem.classList.contains("editMode");
-    //If class of the parent is .editmode
+    var containsClass=listItem.classList.contains("todo-list-section__list-item_edit-mode");
+    //If class of the parent is .todo-list-section__list-item_edit-mode
     if(containsClass){
 
-        //switch to .editmode
+        //switch to .todo-list-section__list-item_edit-mode
         //label becomes the inputs value.
         label.innerText=editInput.value;
         editBtn.innerText="Edit";
@@ -103,8 +103,8 @@ var editTask=function(){
         editBtn.innerText="Save";
     }
 
-    //toggle .editmode on the parent.
-    listItem.classList.toggle("editMode");
+    //toggle .todo-list-section__list-item_edit-mode on the parent.
+    listItem.classList.toggle("todo-list-section__list-item_edit-mode");
 };
 
 
