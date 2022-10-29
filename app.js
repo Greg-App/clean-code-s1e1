06c9cad264a4514ca79563907e711f6a85c2c5b4
Greg App <greg358@mail.ru>
2022-10-29 20:39:57 +0300

    refactor: rename list item elements classnames according to rule advanced 2.1-BEM

--- app.js
+++ app.js
@@ -44,9 +44,9 @@ var createNewTaskElement=function(taskString){
     editInput.className="todo-list-section__input";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="todo-list-section__list-item-edit-btn";
 
-    deleteButton.className="delete";
+    deleteButton.className="todo-list-section__list-item-delete-btn";
     deleteButtonImg.src='./remove.svg';
     deleteButton.appendChild(deleteButtonImg);
 
@@ -87,7 +87,7 @@ var editTask=function(){
 
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
-    var editBtn=listItem.querySelector(".edit");
+    var editBtn=listItem.querySelector(".todo-list-section__list-item-edit-btn");
     var containsClass=listItem.classList.contains("editMode");
     //If class of the parent is .editmode
     if(containsClass){
@@ -159,8 +159,8 @@ var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log("bind list item events");
 //select ListItems children
     var checkBox=taskListItem.querySelector("input[type=checkbox]");
-    var editButton=taskListItem.querySelector("button.edit");
-    var deleteButton=taskListItem.querySelector("button.delete");
+    var editButton=taskListItem.querySelector("button.todo-list-section__list-item-edit-btn");
+    var deleteButton=taskListItem.querySelector("button.todo-list-section__list-item-delete-btn");
 
 
     //Bind editTask to edit button.
